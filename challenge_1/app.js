fd4f518eb3bd479174ed6990aff94054b79dce42
misha <mishall.afzal@gmail.com>
2018-02-22 12:31:52 -0800

    add moves

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,26 @@
-console.log('hi');
+var turn = true;
+
+// adding moves
+function addMove (id)  {
+  if(!document.getElementById(id).innerHTML) {
+    if(turn) {
+      document.getElementById(id).innerHTML = 'X';
+    } else {
+      document.getElementById(id).innerHTML = 'O';
+    }
+    turn = !turn; 
+  } else {
+    alert('this space is already taken, try another spot');
+  }  
+}
+
+// check winner
+
+// click handler 
+document.addEventListener("click", function(e){
+  var id = e.target.id;
+  if(id && id > 0 && id <= 9){
+    addMove(id);
+  }
+});
 
